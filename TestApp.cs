018faed98274e0fd1d144b9610920873978ec846
Tobias <benes.tobik@gmail.com>
2024-01-11 09:47:38 +0100

    Oprava

---
+++ TestApp.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.Metrics;
+
+internal class TestApp
+{
+    public void Run()
+    {
+        Counter();
+    }
+
+    public void Counter()
+    {
+        bool proceed = true;
+        int hodnota = 0;
+
+        while (proceed)
+        {
+            Console.WriteLine("Zadejte číslo");
+            string userInput = Console.ReadLine();
+            Console.WriteLine($"Zadaná hodnota je {userInput}");
+            if (userInput == "exit")
+            {
+                proceed = false;
+            }
+            else 
+            {
+                int input = SafelyConvertToInt(userInput);
+                hodnota += input;
+                Console.WriteLine(hodnota);
+            }
+        }
+    }
+    public int SafelyConvertToInt(string x)
+    {
+        if (int.TryParse(x, out int result))
+        {
+            return result;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
